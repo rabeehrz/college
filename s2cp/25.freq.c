3925a01e8004007b9147ee1d3a35d7e6d0b92a59
rabeehrz <mohammadrabeeh@gmail.com>
2019-05-07 00:32:13 +0530

    Added C325

---
+++ s2cp/25.freq.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 256
+void main() {
+    FILE *fp;
+    char ch;
+    int frequency[MAX_SIZE] = {0}, i;
+    fp = fopen("25.txt", "r");
+    while((ch=getc(fp)) != EOF)
+        frequency[ch]++;
+    fclose(fp);
+    for(i = 0; i < MAX_SIZE; i++) {
+        if(frequency[i] > 0)
+            printf("%c: %d\n", i, frequency[i]);
+    }
+}
